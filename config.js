743ecdddf4b46f53fdd3f302f92cec17dc74f28f
glennmalau <glennrasyamalau@gmail.com>
2024-12-15 18:28:27 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
-global.owner = ['62895331520602']  
-global.mods = ['62895331520602'] 
-global.prems = ['62895331520602']
-global.nameowner = 'Tio'
-global.numberowner = '62895331520602' 
+global.owner = ['62823617833446']  
+global.mods = ['6282361783446'] 
+global.prems = ['6282361783446']
+global.nameowner = 'Bos Zenn'
+global.numberowner = '6282361783446' 
 global.mail = 'support@tioprm.eu.org' 
-global.gc = 'https://chat.whatsapp.com/CP5mV2D9IO730HpBgmkuyi'
-global.instagram = 'https://instagram.com/prm2.0'
-global.wm = '© Tio'
-global.wait = '_*Tunggu sedang di proses...*_'
-global.eror = '_*Server Error*_'
-global.stiker_wait = '*⫹⫺ Stiker sedang dibuat...*'
-global.packname = 'Made With'
-global.author = 'Bot WhatsApp'
+global.gc = 'https://instagram.com/glen_malau'
+global.instagram = 'https://instagram.com/glen_malau'
+global.wm = '© Zenn'
+global.wait = '_*Tunggu ya Bray,Pesan sedang di proses...*_'
+global.eror = '_*Aduh Server kayanya Error deh Bray,Coba lagi nanti atau hubungi owner*_'
+global.stiker_wait = '*⫹⫺ Stiker nya lagi dibuat Bray...*'
+global.packname = '+6285191789894'
+global.author = 'Zenn Botzz'
 global.autobio = false // Set true untuk mengaktifkan autobio
 global.maxwarn = '3' // Peringatan maksimum
 global.antiporn = true // Auto delete pesan porno (bot harus admin)
 
 //INI WAJIB DI ISI!//
-global.btc = 'YOUR_APIKEY_HERE' 
+global.btc = 'gSMATOSu' 
 //Daftar terlebih dahulu https://api.botcahx.eu.org
 
 //INI OPTIONAL BOLEH DI ISI BOLEH JUGA ENGGA//
@@ -31,7 +31,7 @@ global.APIs = {
 
 //ini tidak di isi juga tidak apa-apa
 global.APIKeys = { 
-  'https://api.botcahx.eu.org': 'APIKEY' 
+  'https://api.botcahx.eu.org': 'gSMATOSu' 
 }
 
 let fs = require('fs')
